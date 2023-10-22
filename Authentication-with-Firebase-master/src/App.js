31098277cdcaa3d5d713841d4126180c99e89b3b
Umme Aiman <122363026+umme-aimany@users.noreply.github.com>
2023-10-22 11:53:21 +0500

    Add files via upload

---
+++ Authentication-with-Firebase-master/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { useState } from 'react';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Authentication-with-Firebase-master/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
